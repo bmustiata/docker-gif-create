a1588a52c907794a746cad99beece7cb716ea606
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-05-17 01:14:20 +0200

    Take into the account the scaling.

--- bin/run_conversion.sh
+++ bin/run_conversion.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-# http://eyeofmidas.wordpress.com/2014/06/03/how-to-record-desktop-images-into-gif-format-on-ubuntu-14-04/
-
-# TODO: Show some progress
-# TODO: Stop for "editing"
-
 output=$1
+scaling=$2
 
 if [ -z $output ]; then
   echo "Output not provided"
@@ -26,7 +22,7 @@ echo '-----> [1/2] Extracting pngs'
 avconv -i /tmp/in/test.avi -vsync 1 -an -y -qscale 1 /tmp/out/out_%05d.png
 
 echo '-----> [2/2] Creating the GIF'
-gimp --no-interface --no-data --no-fonts --batch-interpreter=python-fu-eval -b "pdb.python_fu_create_gif('/tmp/out', '/tmp/final.gif', '1.0')" -b "pdb.gimp_quit(1)"
+gimp --no-interface --no-data --no-fonts --batch-interpreter=python-fu-eval -b "pdb.python_fu_create_gif('/tmp/out', '/tmp/final.gif', '$scaling')" -b "pdb.gimp_quit(1)"
 
 cp /tmp/final.gif /tmp/write/$output
 
